f3271ed55a0e176b77643055454d3b9e6d0543ff
prahalladg <prahallad.g@quadwave.com>
2023-03-01 12:07:10 +0530

    Password Generator

---
+++ alltypepassword.py
@@ -0,0 +1,41 @@
+import random
+import string
+import smtplib
+
+def get_password():
+    random_source = string.ascii_letters + string.digits
+    # select 1 lowercase
+    password = random.choice(string.ascii_lowercase)
+    # select 1 uppercase
+    password += random.choice(string.ascii_uppercase)
+    # select 1 digit
+    password += random.choice(string.digits)
+    
+
+    # generate other characters
+    for i in range(7):
+        password += random.choice(random_source)
+    
+    password_list = list(password)
+    # to shuffle characters
+    random.SystemRandom().shuffle(password_list)
+    password = ''.join(password_list)
+    return password
+
+password= get_password()
+
+
+sender = "pgtest1@gmail.com"
+receiver = "pgtest122@gmail.com"
+
+message = f"""\
+Subject: your new password
+To: {receiver}
+From: {sender}
+
+Hi user,
+Your new password is - {password}"""
+
+with smtplib.SMTP("sandbox.smtp.mailtrap.io", 2525) as server:
+    server.login("4f7e2509c9db93", "932c3449ea967e")
+    server.sendmail(sender, receiver, message)
---
+++ randomPasswordGenerator.py
@@ -0,0 +1,31 @@
+import random
+import smtplib
+from email.message import EmailMessage
+
+def generatePassword(n):
+    passwordconbination = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%&*"
+    password = ""
+
+    for i in range(n):
+        password += random.choice(passwordconbination)
+
+    return password
+#n=int(input("Enter the length of password:"))
+n = 8
+password = generatePassword(n)
+
+# set your email and password
+mail_ID = "pgtest122@gmail.com"
+mail_Pwd = "jrjauxmoivzccgqs"  #app password
+
+
+mail_details = EmailMessage()
+mail_details['Subject'] = "Here is your password"
+mail_details['From'] = mail_ID
+mail_details['To'] = "pgahir1045@gmail.com"
+mail_details.set_content("Hi user,\n Your password is "+password)
+
+# send email
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(mail_ID, mail_Pwd)
+    smtp.send_message(mail_details)
